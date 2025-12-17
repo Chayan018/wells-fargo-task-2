7df8fec60a2e7eea4b4a51910768ddd9d26ea199
Chayan Das <chayandas617@gmail.com>
2025-12-17 14:18:31 +0530

    Implemented data model entities

---
+++ src/main/java/com/wellsfargo/counselor/entities/Client.java
@@ -0,0 +1,54 @@
+package com.wellsfargo.counselor.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Client {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long clientId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String phone;
+
+    @ManyToOne
+    @JoinColumn(name = "advisor_id", nullable = false)
+    private Advisor advisor;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Portfolio portfolio;
+
+    public Client(Long clientId, String name, String email, String phone,
+                  Advisor advisor, Portfolio portfolio) {
+        this.clientId = clientId;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.advisor = advisor;
+        this.portfolio = portfolio;
+    }
+
+    public Client() {}
+
+    public Long getClientId() { return clientId; }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+    public String getPhone() { return phone; }
+    public void setPhone(String phone) { this.phone = phone; }
+    public Advisor getAdvisor() { return advisor; }
+    public void setAdvisor(Advisor advisor) { this.advisor = advisor; }
+    public Portfolio getPortfolio() { return portfolio; }
+    public void setPortfolio(Portfolio portfolio) { this.portfolio = portfolio; }
+}
+
+
+
